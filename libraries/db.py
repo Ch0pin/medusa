Ch0pin/medusa

commit ce29bcca2f12d566c7df6ad8c518449cd16a0797

some more optimizations

libraries/db.py
@@ -9,9 +9,9 @@ class apk_db:
         self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
 
         if not self.cursor.fetchall():
-            self.create_db(self.connection)
+            self.create_db()
 
-    def create_db(self, cursor):
+    def create_db(self):
         self.cursor.execute("""CREATE TABLE Application(sha256 TEXT, name TEXT, packageName TEXT, versionCode TEXT, versionName TEXT, minSdkVersion TEXT, targetSdkVersion TEXT, maxSdkVersion TEXT,permissions TEXT, libraries TEXT, debuggable TEXT, allowbackup TEXT, androidManifest TEXT, stringResources TEXT, original_filename TEXT, tampered TEXT)""")
 
         self.cursor.execute("""CREATE TABLE Permissions(app_sha256 TEXT, permission TEXT, type TEXT, shortDescription TEXT, fullDescription TEXT)""")
@@ -31,25 +31,12 @@ class apk_db:
         self.cursor.execute("""CREATE TABLE "Notes" ("index"	INTEGER NOT NULL UNIQUE, "app_sha256"	TEXT NOT NULL, "note"	TEXT, PRIMARY KEY("index" AUTOINCREMENT));""")
 
     def delete_application(self, sha256):
-        sql1 = f"DELETE FROM Application WHERE sha256 = '{sha256}'"
-        sql2 = f"DELETE FROM Permissions WHERE app_sha256 = '{sha256}'"
-        sql3 = f"DELETE FROM Activities WHERE app_sha256 = '{sha256}'"
-        sql4 = f"DELETE FROM Services WHERE app_sha256 = '{sha256}'"
-        sql5 = f"DELETE FROM Providers WHERE app_sha256 = '{sha256}'"
-        sql6 = f"DELETE FROM Receivers WHERE app_sha256 = '{sha256}'"
-        sql7 = f"DELETE FROM ActivityAlias WHERE app_sha256 = '{sha256}'"
-        sql8 = f"DELETE FROM IntentFilters WHERE app_sha256 = '{sha256}'"
-        sql9 = f"DELETE FROM Notes WHERE app_sha256 = '{sha256}'"
+        tables_to_delete = ["Application", "Permissions", "Activities", "Services", "Providers", "Receivers",
+                            "ActivityAlias", "IntentFilters", "Notes"]
 
-        self.cursor.execute(sql1)
-        self.cursor.execute(sql2)
-        self.cursor.execute(sql3)
-        self.cursor.execute(sql4)
-        self.cursor.execute(sql5)
-        self.cursor.execute(sql6)
-        self.cursor.execute(sql7)
-        self.cursor.execute(sql8)
-        self.cursor.execute(sql9)
+        for table in tables_to_delete:
+            query = f"DELETE FROM {table} WHERE sha256 = '{sha256}'"
+            self.cursor.execute(query)
 
         self.connection.commit()
         return
@@ -123,8 +110,7 @@ class apk_db:
         self.cursor.execute(sql)
         if not self.cursor.fetchall():
             return False
-        else:
-            return True
+        return True
 
     def update_activities(self, attribs):
         sql = """INSERT INTO Activities(app_sha256, name, enabled, exported, autoRemoveFromRecents, 

libraries/APKEnum.py
@@ -222,48 +222,32 @@ def findPublicIPs(line):
 
 
 def performRecon(line):
-    exceptions_occured = False
     global domainList, authorityList, inScopeDomainList, inScopeAuthorityList
-    filecontent = ""
+
+    exceptions_occurred = False
+
+    def process_file_content(file_content):
+        findUrls(file_content)
+        findPublicIPs(file_content)
+        findS3Bucket(file_content)
+        findS3Website(file_content)
+        findGoogleAPIKeys(file_content)
+
     for dir_path, dirs, file_names in os.walk(line):
         for file_name in file_names:
+            full_path = os.path.join(dir_path, file_name)
             try:
-                fullpath = os.path.join(dir_path, file_name)
-                fileobj = open(fullpath, mode='r')
-                filecontent = fileobj.read()
-                fileobj.close()
-            except Exception as e:
-                exceptions_occured = True
+                with open(full_path, "r") as f:
+                    file_content = f.read()
+                    thread = threading.Thread(target=process_file_content, args=(file_content,))
+                    thread.start()
+                    thread.join()
+            except Exception:
+                exceptions_occurred = True
 
-            try:
-                # findUrls(filecontent)
-                # findPublicIPs(filecontent)
-                # findS3Bucket(filecontent)
-                # findS3Website(filecontent)
-                # findGoogleAPIKeys(filecontent)
-                # findUnrestrictedGmapKeys()
-                t1 = Thread(target=findUrls, args=(filecontent,))
-                t2 = Thread(target=findPublicIPs, args=(filecontent,))
-                t3 = Thread(target=findS3Bucket, args=(filecontent,))
-                t4 = Thread(target=findS3Website, args=(filecontent,))
-                t5 = Thread(target=findGoogleAPIKeys, args=(filecontent,))
-                t1.start()
-                t2.start()
-                t3.start()
-                t4.start()
-                t5.start()
-                t1.join()
-                t2.join()
-                t3.join()
-                t4.join()
-                t5.join()
-            # t6 = Thread(target=findUnrestrictedGmapKeys, args=())
-            # t6.start()
-            # t6.join()
-            except Exception as e:
-                myPrint("E: Error while spawning threads", "ERROR")
-    if exceptions_occured:
-        print('[E] Some exceptions occured and were ommited !')
+    if exceptions_occurred:
+        print('[E] Some exceptions occurred and were omitted !')
+
     displayResults()
 
 

libraries/Modules.py
@@ -87,7 +87,6 @@ class ModuleManager:
             print(f'Module {moduleName} not found!')
 
     def unstage(self, moduleName):
-        
         tmp = len(self.staged)
 
         for mod in self.staged:  # check for exact name first

libraries/Questions.py
@@ -38,9 +38,9 @@ class Polar:
 
 	def ask(self):
 		answer = input(self.message + (' (Y/n) ' if self.default else ' (y/N) ')).strip()
-		if answer.lower() == 'y' or answer.lower() == 'yes':
+		if answer.lower() in ['y', 'yes']:
 			return True
-		elif answer.lower() == 'n' or answer.lower() == 'no':
+		elif answer.lower() in ['n', 'no']:
 			return False
 		elif answer == '':
 			return True if self.default else False

libraries/libadb.py
@@ -73,17 +73,17 @@ class android_device:
 
     def run_pseudo_adb_root_cmd(self, cmd):
         cmdf = ["adb", "-s", f"{self.id}", "shell", f"echo \"{cmd}\" | su"]
-        proccess = subprocess.Popen(cmdf, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        # output, error = proccess.communicate()
+        process = subprocess.Popen(cmdf, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # output, error = process.communicate()
         line = ""
-        for b in proccess.stdout:
+        for b in process.stdout:
             line += b.decode("utf-8")
         return line
 
     def run_command(self, cmd):
-        proccess = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, error = proccess.communicate()
-        if proccess.returncode != 0:
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = process.communicate()
+        if process.returncode != 0:
             return error
         else:
             return output

libraries/libmango.py
@@ -456,7 +456,7 @@ $adb remount
 
                 """ + RESET)
                 print()
-        except Exception as e:
+        except Exception:
             print('')
 
     def do_jdwp(self, line):
@@ -560,7 +560,7 @@ $adb remount
     def do_notify(self, line):
         """Usage: notify 'notification title' 'Notification body,....'
         Sends a notification to the device (it is used to trigger notification listeners). 
-        Please not that the Medusa Agent must be installed and running on the device 
+        Please note that the Medusa Agent must be installed and running on the device
         (see installagent)."""
 
         try:
@@ -594,7 +594,7 @@ $adb remount
                     print("[!] zipalign is not installed, quiting !")
                     return
                 if not os.path.exists(APKTOOL):
-                    if (Polar('[?] apktool has not been downloaded, do you want to do it now ?').ask()):
+                    if Polar('[?] apktool has not been downloaded, do you want to do it now ?').ask():
                         self.download_file(APKTOOL_URL, APKTOOL)
 
                 print(GREEN + '[+] Unpacking the apk....' + RESET)
@@ -627,7 +627,7 @@ $adb remount
                     shutil.move(file, 'original_' + file)
                     shutil.move(ALLIGNED_APK, file)
 
-                    if (not Polar('[?] Do you want to keep the extracted resources ?').ask()):
+                    if not Polar('[?] Do you want to keep the extracted resources ?').ask():
                         shutil.rmtree(TMP_FOLDER)
             except Exception as e:
                 print(e)
@@ -1305,7 +1305,7 @@ $adb remount
                     print(display_text)
             print(Style.RESET_ALL)
         except Exception as e:
-            print("An error occured")
+            print(f"An error occurred: {e}")
 
     def print_services(self, all=True):
         try:
@@ -1322,7 +1322,7 @@ $adb remount
                     print(display_text)
             print(Style.RESET_ALL)
         except Exception as e:
-            print("An error occured")
+            print(f"An error occurred: {e}")
 
     def print_strings(self):
         for string in self.strings.split('\n'):

libraries/natives.py
@@ -85,7 +85,6 @@ class nativeHandler:
     def getModules(self, package, force):
         print(f'[i] Using device with id {self.device}')
         self.modules = []
-        pid = 0
         try:
             if force:
                 pid = self.device.spawn(package)
@@ -105,7 +104,6 @@ class nativeHandler:
             self.device.resume(pid)
             time.sleep(5)
             script.unload()
-
         except Exception as e:
             print(e)
 
@@ -183,7 +181,7 @@ class nativeHandler:
                 cmd = input(prompt)
             prev_cmd = 'e'
 
-            while (not cmd.lower().startswith('e')):
+            while not cmd.lower().startswith('e'):
                 if cmd.startswith('r@'):
                     cmd = self.read_memory(cmd[2:], script, session, codejs, prolog, epilog, payload, prompt, True,
                                            size)
@@ -277,7 +275,7 @@ class nativeHandler:
             cmd = input(self.prompt_)
             prev_cmd = 'e'
 
-            while (not cmd.lower().startswith('e')):
+            while not cmd.lower().startswith('e'):
                 if cmd.startswith('r@'):
                     cmd = self.read_memory(cmd[2:], script, session, codejs, prolog, epilog, payload, self.prompt_)
                     continue
@@ -315,7 +313,6 @@ class nativeHandler:
                 payload = message["payload"]
                 self.modules.append(payload.split(":")[0].strip())
                 # self.script.post({'input':'null'})
-
         except Exception as e:
             print(e)
 

commit 8ad24388adbbc3276bd604141e743814c6b37682

fixed a bug introduced in commit ce29bcc:mango  was throwing an error when trying to delete an app from the db

libraries/db.py
@@ -31,11 +31,14 @@ class apk_db:
         self.cursor.execute("""CREATE TABLE "Notes" ("index"	INTEGER NOT NULL UNIQUE, "app_sha256"	TEXT NOT NULL, "note"	TEXT, PRIMARY KEY("index" AUTOINCREMENT));""")
 
     def delete_application(self, sha256):
-        tables_to_delete = ["Application", "Permissions", "Activities", "Services", "Providers", "Receivers",
+
+        sql1 = f"DELETE FROM Application WHERE sha256 = '{sha256}'"
+        self.cursor.execute(sql1)
+        tables_to_delete = ["Permissions", "Activities", "Services", "Providers", "Receivers",
                             "ActivityAlias", "IntentFilters", "Notes"]
 
         for table in tables_to_delete:
-            query = f"DELETE FROM {table} WHERE sha256 = '{sha256}'"
+            query = f"DELETE FROM {table} WHERE app_sha256 = '{sha256}'"
             self.cursor.execute(query)
 
         self.connection.commit()

modules/helpers/android_net_uri.med
@@ -41,6 +41,12 @@ Uri.getQueryParameters.implementation = function(key){
     return retval;
 }
 
+Uri.parse.implementation = function(uriString){
+  var ret = this.parse(uriString);
+  colorLog('[+] Uri parse with Uri String: '+uriString,{c: Color.Green})
+  return ret;
+}
+
 Uri.getAuthority.implementation = function(key){
   var ret = this.getAuthority();
   colorLog('[+] getAuthority for Uri: '+this,{c: Color.Green})
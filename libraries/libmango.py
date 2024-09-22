3cd5396f51d28ccd2601376a3a39e795b904d783
alright21 <alberto.giust21@gmail.com>
2024-09-22 19:06:15 +0200

    refactor(mango): add logger in patch

--- libraries/libmango.py
+++ libraries/libmango.py
@@ -607,7 +607,7 @@ $adb remount
         if len(line.arg_list) > 0:
             self.patch_apk(line.arg_list[0])
         else:
-            print(Fore.RED + '[!] Usage: patch /full/path/to/foo.apk' + Fore.RESET)
+            logger.error("[!] Usage: patch /full/path/to/foo.apk")
         
 
 
@@ -620,7 +620,7 @@ $adb remount
             for file in line.arg_list:
                 self.patch_apk(file)
         else:
-            print(Fore.RED + '[!] Usage: patchmultiple /full/path/to/foo.apk /full/path/to/split_config.en.apk ...' + Fore.RESET)
+            logger.error("[!] Usage: patchmultiple /full/path/to/foo.apk /full/path/to/split_config.en.apk ...")
             
 
 
@@ -1708,59 +1708,59 @@ $adb remount
             ALIGNED_APK = file_name + '_debuggable' + extension
             try:
                 if not self.does_exist("apksigner"):
-                    print("[!] apksigner is not installed, quitting !")
+                    logger.error("[!] apksigner is not installed, quitting !")
                     return
                 if not self.does_exist("zipalign"):
-                    print("[!] zipalign is not installed, quitting !")
+                    logger.error("[!] zipalign is not installed, quitting !")
                     return
                 if not os.path.exists(APKTOOL):
                     if Polar('[?] apktool has not been downloaded, do you want to do it now ?').ask():
-                        print(Fore.GREEN + '[+] Downloading apktool from ' + APKTOOL_URL + ' to ' +APKTOOL)
+                        logger.info("[+] Downloading apktool from " + APKTOOL_URL + " to " +APKTOOL)
                         self.download_file(APKTOOL_URL, APKTOOL)
 
-                print(Fore.GREEN + '[+] Unpacking the apk...' + Fore.RESET)
+                logger.info("[+] Unpacking the apk...")
                 if os.path.exists(APP_FOLDER):
                     if Polar('[?] Folder' + APP_FOLDER + ' already exists. Do you want to remove the old resources?').ask():
-                        print(Fore.GREEN + '[+] Removing old resources...' + Fore.RESET)
+                        logger.info("[+] Removing old resources...")
                         shutil.rmtree(APP_FOLDER)
                     else:
-                        print(Fore.RED + '[!] The application will use the existing directory' + Fore.RESET)
+                        logger.info("[!] The application will use the existing directory")
 
                 subprocess.run('java -jar ' + APKTOOL + f' d {file} -o {APP_FOLDER}', shell=True)
 
-                print(Fore.GREEN + '[+] Extracting the manifest...' + Fore.RESET)
+                logger.info("[+] Extracting the manifest...")
                 with open(APP_FOLDER + '/AndroidManifest.xml', 'rt') as f:
                     data = f.read()
 
-                print(Fore.GREEN + '[+] Setting the debug flag to true...' + Fore.RESET)
+                logger.info("[+] Setting the debug flag to true...")
 
                 if 'android:debuggable="true"' in data:
-                    print(Fore.RED + "[!] Application is already debuggable !" + Fore.RESET)
+                    logger.error("[!] Application is already debuggable !")
                 else:
                     data = data.replace(text_to_search, replacement_text)
 
                     with open(APP_FOLDER + '/AndroidManifest.xml', 'wt') as f:
                         f.write(data)
-                    print(Fore.GREEN + '[+] Repacking the app...' + Fore.RESET)
+                    logger.info("[+] Repacking the app...")
                     subprocess.run('java -jar ' + APKTOOL + f' b {APP_FOLDER} -o {DEBUGGABLE_APK}', shell=True)
-                    print(Fore.GREEN + '[+] Aligning the apk file.....' + Fore.RESET)
+                    logger.info("[+] Aligning the apk file...")
                     subprocess.run(f'zipalign -p -v 4 {DEBUGGABLE_APK} {ALIGNED_APK}', shell=True)
-                    print(Fore.GREEN + '[+] Signing the apk.....' + Fore.RESET)
+                    logger.info("[+] Signing the apk...")
                     subprocess.run(
                         f'apksigner sign --ks {SIGNATURE} -ks-key-alias common --ks-pass pass:password --key-pass pass:password  {ALIGNED_APK}',
                         shell=True)
-                    print(Fore.GREEN + '[+] Removing the unsigned apk.....' + Fore.RESET)
+                    logger.info("[+] Removing the unsigned apk...")
                     os.remove(DEBUGGABLE_APK)
-                    print(Fore.GREEN + '[+] Original file: ' + file + Fore.RESET)
-                    print(Fore.GREEN + '[+] Debuggable file: ' + ALIGNED_APK + Fore.RESET) 
+                    logger.info("[+] Original file: " + file)
+                    logger.info("[+] Debuggable file: " + ALIGNED_APK)
 
                 if not Polar('[?] Do you want to keep the extracted resources ?').ask():
                     shutil.rmtree(APP_FOLDER)
                     if len(os.listdir(TMP_FOLDER)) == 0:
-                        print(Fore.GREEN + f'[+] {TMP_FOLDER} is empty, removing it...' + Fore.RESET)
+                        logger.info(f"[+] {TMP_FOLDER} is empty, removing it...")
                         shutil.rmtree(TMP_FOLDER)
             except Exception as e:
-                print(e)
+                logger.error(e)
 
         else:
-            print(Fore.RED + "[!] File doesn't exist." + Fore.RESET)
+            logger.error("[!] File doesn't exist.")
